f23b57fb68efac5b7a82d7e486da7a3c40cc6664
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-11-21 22:53:31 -0500

    need to finish the associaitions, stopped at Product belowngs to:

--- models/index.js
+++ models/index.js
@@ -5,10 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasMany(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: 
+})
 
 // Tags belongToMany Products (through ProductTag)
 
